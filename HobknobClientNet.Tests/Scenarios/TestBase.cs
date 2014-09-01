9db5aac7fa1e257148be3ad5a597662c338d5043
Chris Riddle <christriddle@gmail.com>
2014-09-01 15:41:23 +0100

    Added end-to-end tests for Get and GetOrDefault

---
+++ HobknobClientNet.Tests/Scenarios/TestBase.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using etcetera;
+using NUnit.Framework;
+
+namespace HobknobClientNet.Tests.Scenarios
+{
+    public class TestBase
+    {
+        private readonly EtcdClient _etcdClient;
+        private HobknobClient _hobknobClient;
+        private string _applicationName;
+
+        private const string EtcdHost = "192.168.7.58";
+        private const int EtcdPort = 4001;
+
+        private readonly HashSet<string> _applicationKeysToClearOnTearDown = new HashSet<string>();
+
+        protected TestBase()
+        {
+            _etcdClient = new EtcdClient(new Uri(string.Format("http://{0}:{1}/v2/keys/", EtcdHost, EtcdPort)));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            if (_hobknobClient != null)
+            {
+                _hobknobClient.Dispose();
+            }
+
+            foreach (var application in _applicationKeysToClearOnTearDown)
+            {
+                _etcdClient.DeleteDir("v1/toggles/" + application, true);
+            }
+            _applicationKeysToClearOnTearDown.Clear();
+        }
+
+        protected void Set_application_name(string applicationName)
+        {
+            _applicationName = applicationName;
+        }
+
+        protected void Given_a_toggle(string applicationName, string toggleName, string value)
+        {
+            var key = string.Format("v1/toggles/{0}/{1}", applicationName, toggleName);
+            _etcdClient.Set(key, value);
+            _applicationKeysToClearOnTearDown.Add(applicationName);
+        }
+
+        protected void When_I_get(string toggleName, out bool? value)
+        {
+            _hobknobClient = new HobknobClientFactory().Create(EtcdHost, EtcdPort, _applicationName, TimeSpan.FromMinutes(1));
+            value = _hobknobClient.Get(toggleName);
+        }
+
+        protected void When_I_get_with_default(string toggleName, bool defaultValue,  out bool? value)
+        {
+            _hobknobClient = new HobknobClientFactory().Create(EtcdHost, EtcdPort, _applicationName, TimeSpan.FromMinutes(1));
+            value = _hobknobClient.GetOrDefault(toggleName, defaultValue);
+        }
+    }
+}
--- HobknobClientNet/FeatureToggleProvider.cs
+++ HobknobClientNet/FeatureToggleProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using etcetera;
 
@@ -19,13 +20,29 @@ namespace HobknobClientNet
         {
             var etcdResponse = _etcdClient.Get(_applicationDirectoryKey); // todo: does this support recursive node gets?
 
-            // todo: check for error? node == null?
+            if (etcdResponse.ErrorCode.HasValue)
+            {
+                const int keyNotFoundEtcdErrorCode = 100;
+                if (etcdResponse.ErrorCode == keyNotFoundEtcdErrorCode)
+                {
+                    return Enumerable.Empty<KeyValuePair<string, bool?>>();
+                }
+                throw new Exception("Error getting toggles from etcd: " + etcdResponse.Message);
+            }
+
             return etcdResponse.Node.Nodes.Select(GetKeyValuePair);
         }
 
         private static KeyValuePair<string, bool?> GetKeyValuePair(Node node)
         {
-            return new KeyValuePair<string, bool?>(node.Key, ParseFeatureToggleValue(node.Value));
+            var key = GetKey(node.Key);
+            var featureToggleValue = ParseFeatureToggleValue(node.Value);
+            return new KeyValuePair<string, bool?>(key, featureToggleValue);
+        }
+
+        private static string GetKey(string path)
+        {
+            return path.Split('/').Last();
         }
 
         private static bool? ParseFeatureToggleValue(string value)
