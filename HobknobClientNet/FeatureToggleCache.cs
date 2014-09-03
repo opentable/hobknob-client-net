a7bed90aa89005e3a57b0c6d6ae9720bdbea8dbc
Chris Riddle <christriddle@gmail.com>
2014-09-03 17:21:58 +0100

    Version bump. Moved class into separate file.

--- HobknobClientNet/FeatureToggleCache.cs
+++ HobknobClientNet/FeatureToggleCache.cs
@@ -118,14 +118,4 @@ namespace HobknobClientNet
             }
         }
     }
-
-    public class CacheUpdatedArgs : EventArgs
-    {
-        public IEnumerable<CacheUpdate> Updates { get; private set; }
-
-        public CacheUpdatedArgs(IEnumerable<CacheUpdate> updates)
-        {
-            Updates = updates;
-        }
-    }
 }
