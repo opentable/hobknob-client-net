6c8e1bbca4ee30f87fdae651c57ac47010f7264f
Piotr Bazydlo <pbazydlo@opentable,com>
2016-10-31 17:45:25 +0000

    Removed nameof which is csharp 6

--- HobknobClientNet/HobknobClientFactory.cs
+++ HobknobClientNet/HobknobClientFactory.cs
@@ -18,7 +18,7 @@ namespace HobknobClientNet
             var featureToggleCache = new FeatureToggleCache(featureToggleProvider, cacheUpdateInterval);
             var hobknobClient = new HobknobClient(featureToggleCache, applicationName);
             if (cacheUpdateFailed == null)
-                throw new ArgumentNullException(nameof(cacheUpdateFailed));
+                throw new ArgumentNullException("cacheUpdateFailed", "Cached update handler is emtpy");
             featureToggleCache.CacheUpdateFailed += cacheUpdateFailed;
 
             featureToggleCache.Initialize();
