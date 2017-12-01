6a9b82b110fc50809c471ae3898b77533f8fd985
James Hay <jhay@opentable.com>
2017-12-01 15:22:02 +0000

    Update client to be a multi targeted project producing a nuget package containing clients that target .net 4.5.1 and .netcore 2.0
    Uses docker compose to run teh build container to ensure tests are run and successful before producing a nuget package
    
    Fix issues with the latest release where toggles are not being returned
    correctly due to a change in the key used to retrieve toggles introduced in
    https://github.com/opentable/hobknob-client-net/commit/57f37912ff65ea9a6acc02d8bc081795f6eda643

---
+++ src/HobknobClient.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HobknobClientNet
+{
+    public interface IHobknobClient : IDisposable
+    {
+        event EventHandler<CacheUpdatedArgs> CacheUpdated;
+        event EventHandler<CacheUpdateFailedArgs> CacheUpdateFailed;
+        bool GetOrDefault(string featureName, bool defaultValue);
+        bool GetOrDefault(string featureName, string toggleName, bool defaultValue);
+        Dictionary<string, bool> GetAll();
+    }
+
+    public class HobknobClient : IHobknobClient
+    {
+        public event EventHandler<CacheUpdatedArgs> CacheUpdated;
+        public event EventHandler<CacheUpdateFailedArgs> CacheUpdateFailed;
+
+        private readonly FeatureToggleCache _featureToggleCache;
+        private readonly string _applicationName;
+
+        internal HobknobClient(FeatureToggleCache featureToggleCache, string applicationName)
+        {
+            _featureToggleCache = featureToggleCache;
+            _applicationName = applicationName;
+
+            _featureToggleCache.CacheUpdated += RaiseCacheUpdatedEvent;
+            _featureToggleCache.CacheUpdateFailed += RaiseCacheUpdateFailedEvent;
+        }
+
+        public bool GetOrDefault(string featureName, bool defaultValue)
+        {
+            return GetOrDefault(featureName, null, defaultValue);
+        }
+
+        public bool GetOrDefault(string featureName, string toggleName, bool defaultValue)
+        {
+            return _featureToggleCache.Get(_applicationName, featureName, toggleName)
+                .GetValueOrDefault(defaultValue);
+        }
+
+        public Dictionary<string, bool> GetAll()
+        {
+            return _featureToggleCache.GetAll();
+        }
+
+        private void RaiseCacheUpdatedEvent(object sender, CacheUpdatedArgs eventArgs)
+        {
+            if (CacheUpdated != null)
+            {
+                CacheUpdated(sender, eventArgs);
+            }
+        }
+
+        private void RaiseCacheUpdateFailedEvent(object sender, CacheUpdateFailedArgs eventArgs)
+        {
+            if (CacheUpdateFailed != null)
+            {
+                CacheUpdateFailed(sender, eventArgs);
+            }
+        }
+
+        public void Dispose()
+        {
+            _featureToggleCache.CacheUpdated -= RaiseCacheUpdatedEvent;
+            _featureToggleCache.CacheUpdateFailed -= RaiseCacheUpdateFailedEvent;
+
+            if (_featureToggleCache != null)
+            {
+                _featureToggleCache.Dispose();
+            }
+        }
+    }
+}
